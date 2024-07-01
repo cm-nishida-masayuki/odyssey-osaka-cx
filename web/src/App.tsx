1b5cdc0dde2220e5898b02b5dafde6134e4dda20
diggymo <morifuji.kodai@classmethod.jp>
2024-07-01 19:27:31 +0900

    web: useSwrを導入

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
 import { Button } from "@mui/material";
+import { useFetchPokemon } from "./api/pokemon";
 
 function App() {
+  const { pokemon, isLoading, isError } = useFetchPokemon();
+
+  if (isLoading || pokemon === undefined) {
+    return <p>Loading...</p>;
+  }
+  if (isError) {
+    return <p>Error!</p>;
+  }
+
   return (
     <>
+      {pokemon.name}
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       <Button variant="contained">Hello world</Button>
     </>
   );
