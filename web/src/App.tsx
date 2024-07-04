64c3fcdb05d28b45f8c5eacd0c849cfe232b06e9
Shimamura Seiya <shimamura.seiya@classmethod.jp>
2024-07-04 14:18:02 +0900

    Create session view ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,38 @@
+<<<<<<< Updated upstream
 import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const SamplePage = lazy(() => import("./pages/Sample"));
+=======
+import {
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import { deepPurple, grey } from "@mui/material/colors";
+import Header from "./components/Header";
+import BreakItem from "./components/SessionView/BreakList";
+import SessionListItem from "./components/SessionView/SessionItem";
+import SessionList from "./components/SessionView/SessionList";
+import TimeBorder from "./components/SessionView/TimeBorder";
+
+const App = () => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: grey[900],
+      },
+      secondary: {
+        main: deepPurple[900],
+      },
+    },
+  });
+>>>>>>> Stashed changes
 
 export const App = () => {
   return (
+<<<<<<< Updated upstream
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SamplePage />} />
@@ -12,4 +40,40 @@ export const App = () => {
     </BrowserRouter>
   );
 };
+=======
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Typography
+        variant="h6"
+        sx={{
+          fontSize: "24px",
+          fontWeight: "bold",
+          margin: "12px 16px",
+        }}
+      >
+        セッション
+      </Typography>
+      <SessionList>
+        <TimeBorder time="11:00" />
+        <SessionListItem
+          title="セッションのタイトルが入ります。長い可能性があります。"
+          speaker={`クラスメソッド株式会社製造\nビジネステクノロジー部 山田 太郎`}
+        />
+        <TimeBorder time="11:50" />
+        <BreakItem />
+        <TimeBorder time="12:00" />
+        <SessionListItem
+          title="セッションのタイトルが入ります。長い可能性があります。"
+          speaker={`クラスメソッド株式会社製造\nビジネステクノロジー部 山田 太郎`}
+        />
+        <TimeBorder time="12:50" />
+        <BreakItem />
+        <TimeBorder time="13:00" />
+      </SessionList>
+    </ThemeProvider>
+  );
+};
+
+>>>>>>> Stashed changes
 export default App;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+<<<<<<< Updated upstream
 import App from "./app.tsx";
+=======
+import App from "./App.tsx";
+>>>>>>> Stashed changes
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
