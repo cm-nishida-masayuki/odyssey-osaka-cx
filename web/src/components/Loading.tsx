7345bdc43e1d84a11604a5e7e8b222a45351fa98
Shimamura Seiya <shimamura.seiya@classmethod.jp>
2024-07-10 18:34:46 +0900

    Update UI of Loading View

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -1,16 +1,50 @@
 import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const Loading = () => {
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prevDots) => {
+        if (prevDots.length >= 3) {
+          return "";
+        }
+        return prevDots + ".";
+      });
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Box
+      width="100%"
+      height="100%"
       display="flex"
-      alignItems="center"
       justifyContent="center"
-      textAlign="center"
-      paddingTop="32px"
+      paddingTop="160px"
     >
-      Loading...
+      <div
+        style={{
+          fontFamily: "Kanit, sans-serif",
+          // fontSize: "16px",
+          position: "relative",
+        }}
+      >
+        Loading
+        <span
+          style={{
+            position: "absolute",
+            textAlign: "left",
+            marginLeft: "4px",
+          }}
+        >
+          {dots}
+        </span>
+      </div>
     </Box>
   );
 };
+
 export default Loading;
