07e18ce84d012eef0050f2b35bda8fe3a3245733
KazumasaSako <kazumasa.pcs@gmail.com>
2024-07-11 18:35:28 +0900

    feat:アンケート一覧のAPI繋ぎこみ

--- web/src/pages/QuestionnaireListPage.tsx
+++ web/src/pages/QuestionnaireListPage.tsx
@@ -1,11 +1,26 @@
 import { Box } from "@mui/material";
+import Loading from "../components/Loading";
 import { QuestionnaireList } from "../components/QuestionnaireItem";
+import { useQuestionnaires } from "../hooks/useQuestionnaires";
 
 export const QuestionnaireListPage = () => {
+  const [{ data, error, isLoading }] = useQuestionnaires();
+
+  if (isLoading || data === undefined) {
+    return <Loading />;
+  }
+  if (error) {
+    return <div>Error...</div>;
+  }
+
   return (
     <Box padding={"24px"} display="flex" flexDirection="column" gap="12px">
-      <QuestionnaireList />
-      <QuestionnaireList />
+      {data.questionnaires.map((questionnaire) => (
+        <QuestionnaireList
+          key={questionnaire.questionnaireId}
+          {...questionnaire}
+        />
+      ))}
     </Box>
   );
 };
