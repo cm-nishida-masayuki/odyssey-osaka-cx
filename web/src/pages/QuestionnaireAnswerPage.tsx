f46a8407e36a4266c4da1c43b5b2d49714f0d49c
cm-nishida-masayuki <nishida.masayuki@classmethod.jp>
2024-07-15 21:10:48 +0900

    refactor: アンケート結果画面のWebSocket周りの処理をリファクタ

--- web/src/pages/QuestionnaireAnswerPage.tsx
+++ web/src/pages/QuestionnaireAnswerPage.tsx
@@ -1,32 +1,22 @@
-import { Box, Container, Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import { PieChart } from "@mui/x-charts";
-import {
-  Answers,
-  useQuestionnaireAnswers,
-} from "../hooks/useQuestionnaireAnswers";
-import { useMemo, useState } from "react";
-import { useQuestionnaireEvent } from "../hooks/useQuestionnaireEvent";
+import { useMemo } from "react";
 import { QuestionnaireAnswerItem } from "../components/QuestionnaireAnswer/QuestionnaireAnswerItem";
+import { useQuestionnaireAnswers } from "../hooks/useQuestionnaireAnswers";
+import { useQuestionnaireEvent } from "../hooks/useQuestionnaireEvent";
 
 export const QuestionnaireAnswerPage = () => {
   const [{ data }] = useQuestionnaireAnswers({ questionnaireId: 1 });
   const { data: newData } = useQuestionnaireEvent({ questionnaireId: "1" });
-  const [answers, setAnswers] = useState<Answers | undefined>(data);
 
   const choiceCounts = useMemo(() => {
-    if (answers == null && data == null) {
+    if (data == null) {
       return null;
     }
 
-    if (answers == null && data != null) {
-      return setAnswers(data);
-    }
+    const allAnswers = [...data.answers, ...(newData?.answers || [])];
 
-    const allAnswers = [...answers!.answers, ...(newData?.answers || [])];
-
-    setAnswers({ answers: allAnswers });
-
-    return answers?.answers.reduce(
+    return allAnswers.reduce(
       (acc, curr) => {
         acc[curr.choice] = (acc[curr.choice] || 0) + 1;
         return acc;
