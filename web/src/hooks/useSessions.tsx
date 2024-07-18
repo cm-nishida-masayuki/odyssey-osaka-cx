f77ff5fb0dde7a5d0cd818941e1a4d215b795eba
KazumasaSako <kazumasa.pcs@gmail.com>
2024-07-18 20:42:57 +0900

    feat/セッション一覧に進行中のセッションを追加

--- web/src/hooks/useSessions.tsx
+++ web/src/hooks/useSessions.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
 import useSWR from "swr";
 import { config } from "../config";
 
@@ -22,10 +23,49 @@ export const useSessions = () => {
     `${config.API_URL}/sessions`,
     fetcher
   );
+  const [currentSessions, setCurrentSessions] = useState<Sessions["sessions"]>(
+    []
+  );
+
+  // data.sessionsを開始時間順にソート
+  const sessions = useMemo((): Sessions["sessions"] => {
+    if (data === undefined) {
+      return [];
+    }
+    return data.sessions.slice().sort((a, b) => {
+      return new Date(a.startAt).getTime() - new Date(b.startAt).getTime();
+    });
+  }, [data]);
+
+  const getCurrentSessions = useCallback(() => {
+    if (data && data.sessions) {
+      const now = new Date("2024-07-31 17:00:00");
+      const filteredSessions = data.sessions.filter((session) => {
+        const startAt = new Date(session.startAt);
+        const endAt = new Date(session.endAt);
+        return startAt <= now && now < endAt;
+      });
+      setCurrentSessions(filteredSessions);
+    }
+  }, [data]);
+
+  // 1分毎に、現在のセッションを取得
+  useEffect(() => {
+    getCurrentSessions();
+    const interval = setInterval(() => {
+      getCurrentSessions();
+    }, 60000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [getCurrentSessions]);
 
   return [
     {
-      data,
+      data: {
+        sessions: sessions,
+        currentSessions: currentSessions,
+      },
       isLoading,
       error,
     },
--- web/src/pages/SessionListPage.tsx
+++ web/src/pages/SessionListPage.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
+import Loading from "../components/Loading";
 import { SessionItem } from "../components/SessionView/SessionItem";
 import { useSessions } from "../hooks/useSessions";
-import Loading from "../components/Loading";
 
 export const SessionListPage = () => {
   const [{ data, isLoading, error }] = useSessions();
@@ -17,33 +17,36 @@ export const SessionListPage = () => {
     <>
       <>
         <Box paddingX="24px" paddingTop="32px">
-          {/* <Box
-            marginBottom={"24px"}
-            display="flex"
-            flexDirection="column"
-            gap="12px"
-          >
-            <h2
-              style={{
-                fontSize: "20px",
-                fontWeight: "700",
-                lineHeight: "27.24px",
-                textAlign: "left",
-                margin: "0",
-              }}
+          {0 < data.currentSessions.length && (
+            <Box
+              marginBottom={"24px"}
+              display="flex"
+              flexDirection="column"
+              gap="12px"
             >
-              進行中のセッション
-            </h2>
-
-            <SessionItem
-              id={"1"}
-              key={1}
-              startAt={new Date()}
-              endAt={addMinutes(new Date(), 90)}
-              speakerTitle="セッションのタイトルが入ります。長い可能性があります。"
-              speakerName="大阪太郎"
-            />
-          </Box> */}
+              <h2
+                style={{
+                  fontSize: "20px",
+                  fontWeight: "700",
+                  lineHeight: "27.24px",
+                  textAlign: "left",
+                  margin: "0",
+                }}
+              >
+                進行中のセッション
+              </h2>
+              {data.currentSessions.map((session) => (
+                <SessionItem
+                  id={`${session.sessionId}`}
+                  key={session.sessionId}
+                  startAt={new Date(session.startAt)}
+                  endAt={new Date(session.endAt)}
+                  speakerTitle={session.sessionTitle}
+                  speakerName={session.speakerName}
+                />
+              ))}
+            </Box>
+          )}
 
           <Box display="flex" flexDirection="column" gap="12px">
             <h2
