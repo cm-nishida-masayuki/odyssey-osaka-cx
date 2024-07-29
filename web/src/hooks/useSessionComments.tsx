d51ca9b17d99d1c81f0c742b2deab524b55af45c
KazumasaSako <kazumasa.pcs@gmail.com>
2024-07-29 19:32:28 +0900

    feat:コメントをwsとrestで結合して返すように変更

--- web/src/hooks/useSessionComments.tsx
+++ web/src/hooks/useSessionComments.tsx
@@ -1,6 +1,7 @@
+import axios from "axios";
+import { useEffect, useMemo, useRef, useState } from "react";
 import useSWR from "swr";
 import { config } from "../config";
-import axios from "axios";
 import { useLocalStore } from "./useLocalStore";
 
 const fetcher = (key: string) => fetch(key).then((res) => res.json());
@@ -15,14 +16,70 @@ export type Comments = {
   }[];
 };
 
+const useCommentEvent = ({ sessionId }: { sessionId: number }) => {
+  const ws = useRef<WebSocket | null>(null);
+  const [data, setData] = useState<Comments>();
+
+  useEffect(() => {
+    ws.current = new WebSocket(config.WS_URL);
+
+    ws.current.onopen = () => {
+      ws.current?.send(
+        JSON.stringify({
+          action: "hello",
+          id: sessionId,
+          type: "SESSION",
+        })
+      );
+      console.log("WebSocket connected");
+    };
+
+    ws.current.onmessage = (event) => {
+      const inComingComments = JSON.parse(event.data) as Comments;
+
+      setData((prev) => {
+        if (prev == null) {
+          return inComingComments;
+        }
+
+        return {
+          comments: [...prev.comments, ...inComingComments.comments],
+        };
+      });
+    };
+
+    ws.current.onclose = () => {
+      console.log("WebSocket disconnected");
+    };
+
+    return () => {
+      ws.current?.close();
+    };
+  }, [sessionId]);
+
+  return {
+    data,
+  };
+};
+
 export const useSessionComments = ({ sessionId }: { sessionId: number }) => {
   const [participantId] = useLocalStore<string>("participantId");
   const [participantName] = useLocalStore<string>("participantName");
 
+  const { data: newData } = useCommentEvent({ sessionId });
   const { data, error, isLoading } = useSWR<Comments>(
     `${config.API_URL}/sessions/${sessionId}/comments`,
     fetcher
   );
+
+  const joinData = useMemo(() => {
+    if (data == null) return;
+    const allComments = [...data.comments, ...(newData?.comments || [])];
+    return allComments.sort(
+      (a, b) =>
+        new Date(b.commentAt).getTime() - new Date(a.commentAt).getTime()
+    );
+  }, [newData, data]);
 
   const handlePostComments = async ({ comment }: { comment: string }) => {
     await axios.post(`${config.API_URL}/sessions/${sessionId}/comments`, {
@@ -34,7 +91,7 @@ export const useSessionComments = ({ sessionId }: { sessionId: number }) => {
 
   return [
     {
-      data,
+      data: joinData,
       isLoading,
       error,
     },
